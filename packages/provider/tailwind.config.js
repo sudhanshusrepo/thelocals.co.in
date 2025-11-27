3054b83d6b6f4c1c8f4a26669cef7b8915d27a1f
881dhan <881dhan@gmail.com>
2025-11-27 03:43:08 +0000

    fix(provider): configure tailwind and update node version

---
+++ packages/provider/tailwind.config.js
@@ -0,0 +1,13 @@
+
+      /** @type {import('tailwindcss').Config} */
+      export default {
+        content: [
+          "./index.html",
+          "./{components,contexts,services,utils}/**/*.{js,ts,jsx,tsx}",
+        ],
+        theme: {
+          extend: {},
+        },
+        plugins: [],
+      }
+    
